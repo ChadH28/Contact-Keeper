0accb605472f14e6a785cf73dbff60526a2ee9f3
Chad <61545754+ChadH28@users.noreply.github.com>
2021-02-12 13:13:47 +0200

    getting ready to deploy

---
+++ client/build/precache-manifest.1317b79bd9b3b83307487780f370684a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac59da22c0b987bb6bf0f4ef5bf91f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e339d00f6db52a5f54ec",
+    "url": "/static/css/main.50575fe4.chunk.css"
+  },
+  {
+    "revision": "ca51f661263c6cc70df4",
+    "url": "/static/js/2.7b43791f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7b43791f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e339d00f6db52a5f54ec",
+    "url": "/static/js/main.7922715e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // bringing in express server
-const express = require('express');
-const connectDB = require('./Config/db');
-
+const express = require("express");
+const connectDB = require("./Config/db");
 
 // initialzing express into a variable
 const app = express();
@@ -10,20 +9,30 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({extended: false}))
+app.use(express.json({ extended: false }));
 
 // adding endpoint /route
-app.get('/', (req,res) => res.json({msg: "Welcome to the contact keeper api"}))
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the contact keeper api" })
+);
 
 // Defining Routes
-app.use('/api/users', require('./Routes/users'))
-app.use('/api/auth', require('./Routes/auth'))
-app.use('/api/contacts', require('./Routes/contacts'))
+app.use("/api/users", require("./Routes/users"));
+app.use("/api/auth", require("./Routes/auth"));
+app.use("/api/contacts", require("./Routes/contacts"));
 
+// Always put below the routes
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // made a enviromental variable
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`));
-
-
